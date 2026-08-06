89efd53f31450506295a4d8eec9b07ff11744f6b
Vinit Tomar <vinit.tomar68@gmail.com>
2026-08-06 21:06:36 +0530

    v0.12 Code Splitting (lazy loading) done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+import { lazy, Suspense } from "react";
 import Header from "@/components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
-import Cart from "@/pages/Cart";
 import NotFound from "@/pages/NotFound";
-import ProductDetail from "@/pages/ProductDetail";
-import Orders from "@/pages/Orders";
-import Profile from "@/pages/Profile";
+import Spinner from "@/components/Spinner";
+
+const ProductDetail = lazy(() => import("@/pages/ProductDetail"));
+const Cart = lazy(() => import("@/pages/Cart"));
+const Orders = lazy(() => import("@/pages/Orders"));
+const Profile = lazy(() => import("@/pages/Profile"));
+const AdminDashboard = lazy(() => import("@/pages/AdminDashboard"));
 
 function App() {
   return (
     <>
       <Header />
       <main className="bg-gray-50 min-h-screen dark:bg-gray-900">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </main>
     </>
   );
